6bcca10b8e627fdf7952ca1afb682dcf35f1e9de
ksuzushima <cheap_thrills_50102@yahoo.co.jp>
2024-01-07 02:46:59 +0900

    Format

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://ksuzushima.github.io",
+  site: 'https://ksuzushima.github.io',
 });
